cfe8f6d7cfad4769d57f9607414a22d3321dda7e
Miriam Popa <Miriam.Popa@endava.com>
2022-06-28 16:04:09 +0300

    changes for spring app

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/welcome")
+    public String index() {
+        return "ajax";
+    }
+
+}
